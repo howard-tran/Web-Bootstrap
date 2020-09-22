a13858d3b7f61bde927920bec43d5a2ba2f92f43
mingkhoi <mingkhoitran1234@gmail.com>
2020-09-22 19:04:30 +0700

    add jquery support

--- dist/drum.js
+++ dist/drum.js
@@ -3,28 +3,27 @@ function playAudio(fileName) {
     _audio.play();
 }
 function initAudioClickBtn() {
-    var btnParent = document.querySelector(".set");
-    btnParent.querySelector("#w-drum").onclick = function () {
+    $("#w-drum").on("click", function () {
         playAudio("../sounds/tom-1.mp3");
-    };
-    btnParent.querySelector("#a-drum").onclick = function () {
+    });
+    $("#a-drum").on("click", function () {
         playAudio("../sounds/tom-2.mp3");
-    };
-    btnParent.querySelector("#s-drum").onclick = function () {
+    });
+    $("#s-drum").on("click", function () {
         playAudio("../sounds/tom-3.mp3");
-    };
-    btnParent.querySelector("#d-drum").onclick = function () {
+    });
+    $("#d-drum").on("click", function () {
         playAudio("../sounds/tom-4.mp3");
-    };
-    btnParent.querySelector("#j-drum").onclick = function () {
+    });
+    $("#j-drum").on("click", function () {
         playAudio("../sounds/crash.mp3");
-    };
-    btnParent.querySelector("#k-drum").onclick = function () {
+    });
+    $("#k-drum").on("click", function () {
         playAudio("../sounds/kick-bass.mp3");
-    };
-    btnParent.querySelector("#l-drum").onclick = function () {
+    });
+    $("#l-drum").on("click", function () {
         playAudio("../sounds/snare.mp3");
-    };
+    });
 }
 function initAudioTypingBtn() {
     document.addEventListener("keydown", function (e) {
@@ -32,37 +31,37 @@ function initAudioTypingBtn() {
         switch (e.key) {
             case "w":
             case "W": {
-                btnParent.querySelector("#w-drum").click();
+                $("#w-drum").trigger("click");
                 break;
             }
             case "a":
             case "A": {
-                btnParent.querySelector("#a-drum").click();
+                $("#a-drum").trigger("click");
                 break;
             }
             case "s":
             case "S": {
-                btnParent.querySelector("#s-drum").click();
+                $("#s-drum").trigger("click");
                 break;
             }
             case "d":
             case "D": {
-                btnParent.querySelector("#d-drum").click();
+                $("#d-drum").trigger("click");
                 break;
             }
             case "j":
             case "J": {
-                btnParent.querySelector("#j-drum").click();
+                $("#j-drum").trigger("click");
                 break;
             }
             case "k":
             case "K": {
-                btnParent.querySelector("#k-drum").click();
+                $("#k-drum").trigger("click");
                 break;
             }
             case "l":
             case "L": {
-                btnParent.querySelector("#l-drum").click();
+                $("#l-drum").trigger("click");
                 break;
             }
         }
@@ -71,10 +70,4 @@ function initAudioTypingBtn() {
 (function main() {
     initAudioClickBtn();
     initAudioTypingBtn();
-    var _human = {
-        name: "hello",
-        age: 123,
-        fam_name: ["123", "23"],
-    };
-    console.log(_human);
 })();
--- src/drum.ts
+++ src/drum.ts
@@ -4,29 +4,33 @@ function playAudio(fileName: string) {
 }
 
 function initAudioClickBtn() {
-  let btnParent = document.querySelector(".set");
-
-  (btnParent.querySelector("#w-drum") as HTMLElement).onclick = function () {
+  $("#w-drum").on("click", function () {
     playAudio("../sounds/tom-1.mp3");
-  };
-  (btnParent.querySelector("#a-drum") as HTMLElement).onclick = function () {
+  });
+
+  $("#a-drum").on("click", function () {
     playAudio("../sounds/tom-2.mp3");
-  };
-  (btnParent.querySelector("#s-drum") as HTMLElement).onclick = function () {
+  });
+
+  $("#s-drum").on("click", function () {
     playAudio("../sounds/tom-3.mp3");
-  };
-  (btnParent.querySelector("#d-drum") as HTMLElement).onclick = function () {
+  });
+
+  $("#d-drum").on("click", function () {
     playAudio("../sounds/tom-4.mp3");
-  };
-  (btnParent.querySelector("#j-drum") as HTMLElement).onclick = function () {
+  });
+
+  $("#j-drum").on("click", function () {
     playAudio("../sounds/crash.mp3");
-  };
-  (btnParent.querySelector("#k-drum") as HTMLElement).onclick = function () {
+  });
+
+  $("#k-drum").on("click", function () {
     playAudio("../sounds/kick-bass.mp3");
-  };
-  (btnParent.querySelector("#l-drum") as HTMLElement).onclick = function () {
+  });
+
+  $("#l-drum").on("click", function () {
     playAudio("../sounds/snare.mp3");
-  };
+  });
 }
 
 function initAudioTypingBtn() {
@@ -35,57 +39,44 @@ function initAudioTypingBtn() {
     switch (e.key) {
       case "w":
       case "W": {
-        (btnParent.querySelector("#w-drum") as HTMLElement).click();
+        $("#w-drum").trigger("click");
         break;
       }
       case "a":
       case "A": {
-        (btnParent.querySelector("#a-drum") as HTMLElement).click();
+        $("#a-drum").trigger("click");
         break;
       }
       case "s":
       case "S": {
-        (btnParent.querySelector("#s-drum") as HTMLElement).click();
+        $("#s-drum").trigger("click");
         break;
       }
       case "d":
       case "D": {
-        (btnParent.querySelector("#d-drum") as HTMLElement).click();
+        $("#d-drum").trigger("click");
         break;
       }
       case "j":
       case "J": {
-        (btnParent.querySelector("#j-drum") as HTMLElement).click();
+        $("#j-drum").trigger("click");
         break;
       }
       case "k":
       case "K": {
-        (btnParent.querySelector("#k-drum") as HTMLElement).click();
+        $("#k-drum").trigger("click");
         break;
       }
       case "l":
       case "L": {
-        (btnParent.querySelector("#l-drum") as HTMLElement).click();
+        $("#l-drum").trigger("click");
         break;
       }
     }
   });
 }
 
-interface human {
-  name: string;
-  age: number;
-  fam_name: string[] | number[];
-}
-
 (function main() {
   initAudioClickBtn();
   initAudioTypingBtn();
-
-  const _human: human = {
-    name: "hello",
-    age: 123,
-    fam_name: ["123", "23"],
-  };
-  console.log(_human);
 })();
